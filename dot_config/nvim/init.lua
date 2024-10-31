6a3e0dc82c3f8c8a9c1cc8c3c7b2a507e954380f
greenj3lly <ngreen@jellyvision.com>
2024-10-31 16:20:08 -0700

    add nvim markdown, comment handling, gitblame in status

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,2 +1,12 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.g.gitblame_display_virtual_text = 0 -- Disable virtual text
+local git_blame = require("gitblame")
+
+require("lualine").setup({
+  sections = {
+    lualine_c = {
+      { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available },
+    },
+  },
+})
