821e0c388e09efb0759934d56e1d3a65e9ebb8db
greenj3lly <ngreen@jellyvision.com>
2025-03-18 14:41:53 -0700

    add zsh config chezmoi
    rm secrets

---
+++ dot_zsh/aliases.zsh
@@ -0,0 +1,9 @@
+alias zshconfig="$EDITOR ~/.zshrc"
+alias zshenv="$EDITOR ~/.zshenv"
+alias awsconfig="$EDITOR ~/.aws/config"
+alias ohmyzsh="$EDITOR ~/.oh-my-zsh"
+
+alias la="ll -a"
+alias gs="git status"
+alias docker-compose="docker compose"
+
